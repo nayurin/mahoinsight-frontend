39d5ba709b5457d9c65ce0edf575c4096e50434c
nayurin <nayurin@outlook.com>
2020-06-13 00:24:29 +0800

    修改footer增加icp备案信息

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,6 @@
 <template>
   <v-footer
+    v-if="!$store.state.mobile"
     app
     padless
     dark
@@ -7,10 +8,26 @@
     class="font-weight-medium"
   >
     <v-col
-      class="text-center"
-      cols="12"
+      class="text-center col-12"
     >
       MahoMaho INSIGHT!! @ {{ this.$store.state.gitcommit.toUpperCase() }}
+      <br>
+      <span>
+        <a
+          id="footer-icp"
+          href="http://www.beian.miit.gov.cn/"
+        >
+          浙ICP备20021016号
+        </a>
+      </span>
     </v-col>
   </v-footer>
 </template>
+
+<style scoped>
+a#footer-icp {
+  text-decoration: none;
+  color: white;
+  font-size: 0.75em;
+}
+</style>
