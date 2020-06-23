23930410049eff60ef356cd09fb2ec2ed4c44f28
nayurin <nayurin@outlook.com>
2020-06-23 15:10:01 +0800

    fix: 修复vuex参数和缓存不匹配时页面报错bug

--- src/components/princesses/PrincessEquipment.vue
+++ src/components/princesses/PrincessEquipment.vue
@@ -87,7 +87,20 @@ export default {
       return this.$store.getters.curRank
     },
     promotion () {
-      return this.princess.promotion_info
+      const promotion = this.princess.promotion_info
+      for (let i = this.curRank; i >= 1; i--) {
+        if (!promotion[i]) {
+          this.$set(promotion, i, this.unknownRank)
+        }
+      }
+      return promotion
+    },
+    unknownRank () {
+      const promotion = {}
+      Array(1, 2, 3, 4, 5, 6).map(x => {
+        promotion[`equip_slot_${x}`] = 999999
+      })
+      return promotion
     }
   },
   watch: {
