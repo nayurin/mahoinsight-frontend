3117ad448430f30eef3bbcb6a8200c967e875931
nayurin <nayurin@outlook.com>
2020-07-31 13:40:51 +0800

    add: 单元测试基本配置

---
+++ tests/unit/jest.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+
+module.exports = {
+  preset: '@vue/cli-plugin-unit-jest',
+  rootDir: path.resolve(__dirname, '../../'),
+  moduleFileExtensions: [
+    'js',
+    'json',
+    'vue',
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/tests/unit/__mocks__/fileres.js',
+    '\\.(css|less|scss|sass)$': '<rootDir>/tests/unit/__mocks__/style.js'
+  },
+  transform: {
+    '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
+    '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest',
+  },
+  testMatch: [
+    '<rootDir>/tests/unit/specs/*.spec.js'
+  ],
+  testPathIgnorePatterns: [],
+  setupFiles: ['<rootDir>/tests/unit/setup.js'],
+  // coverageDirectory: '<rootDir>/test/coverage',
+  // coverageReporters: ["html", "text-summary"],
+  // collectCoverage: true,
+  // collectCoverageFrom: [
+  //   'src/**/*.{js,vue}',
+  //   '!src/main.js',
+  //   '!src/router/index.js',
+  //   '!**/node_modules/**',
+  // ]
+}
